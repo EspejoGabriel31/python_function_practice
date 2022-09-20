3f800069fd1b042bbab2ee0c6fb9466b3e6bc8c3
unknown <espejo.gabriel779@gmail.com>
2022-09-20 13:37:25 -0700

    pascal function

--- function_practice.py
+++ function_practice.py
@@ -21,9 +21,31 @@ def rev_string(str):
 def num_within(n, br, er):
     return n > br and n <= er
 
+triangle = [[1],[1,1]]
 def pascal(n):
-    triangle = [[1],[[1],[1]]]
-    if(n < 1):
+    if n < 1:
         print('invalid')
-    elif(n == 1):
-        print([1])
+    elif n is 1:
+        print(triangle[0])
+    else:
+        row = 2
+        while len(triangle) < n:
+            n_row = []
+            p_row = triangle[row -1]
+            length = len(p_row) + 1
+            for i in range(length):
+                if i is 0:
+                    n_row.append(1)
+                elif i > 0 and i < length - 1:
+                    n_row.append(triangle[row-1][i-1] + triangle[row-1][i])
+                else:
+                    n_row.append(1)
+            triangle.append(n_row)
+            row += 1
+
+    for row in triangle:
+        print(row)
+
+pascal(2)
+print()
+pascal(7)
